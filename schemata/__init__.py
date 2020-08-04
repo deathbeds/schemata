5e5a2cff64fc51719a772e9bbe6922db681f7e36
tonyfast <tony.fast@gmail.com>
2020-08-03 21:23:15 -0400

    add some protocols and tests

--- schemata/__init__.py
+++ schemata/__init__.py
@@ -3,7 +3,12 @@ __version__ = "0.0.1"
 
 from . import data
 from . import core
+from . import protocols
+from . import types
+from .types import *
+from . import geo
+from . import strings
+
 from .core import *
 from .types import *
-from .geo import *
-#from . import strings
+
---
+++ schemata/tests/test_protocols.py
@@ -0,0 +1,8 @@
+import schemata
+
+
+def test_pandas():
+    df = __import__("pandas").util.testing.makeDataFrame()
+    assert isinstance(df, schemata.protocols.dataframe)
+    assert isinstance(df.values, schemata.protocols.numpy)
+    assert not isinstance(df, schemata.protocols.series)
