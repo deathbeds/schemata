45925cb14cd3c8c74b2243e06c21eeb0f8d8ff07
tonyfast <tony.fast@gmail.com>
2020-08-03 21:23:00 -0400

    flake

--- schemata/tests/test_core.py
+++ schemata/tests/test_core.py
@@ -7,13 +7,17 @@ import schemata
 def test_invalid_schema():
     with pytest.raises(jsonschema.ValidationError):
 
-        class t(schemata.jsonschema):
+        class s(schemata.jsonschema):
             type: "whatever"
+
+        s
 
     with pytest.raises(jsonschema.ValidationError):
 
         class t(schemata.object):
             properties: []
+
+        t
 
 
 def test_compare_types():
