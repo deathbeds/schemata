4a9796fbfba565b923b3c2c6d1d44cf006daddc8
tonyfast <tony.fast@gmail.com>
2020-08-07 18:50:57 -0400

    add localizing validator

--- schemata/checker.py
+++ schemata/checker.py
@@ -1,6 +1,9 @@
-"""The checker module defines locale aware jsonschema type and format checkers"""
+"""The checker module defines locale aware jsonschema type and format checkers.
 
-import jsonschema, json, munch, frozendict
+It provides a localizing Validator for different locales."""
+
+import jsonschema, json, frozendict, pathlib
+from . import util, translate
 
 
 def is_array(validator, object):
@@ -8,7 +11,7 @@ def is_array(validator, object):
 
 
 def is_object(validator, object):
-    return isinstance(object, (frozendict.frozendict, munch.Munch, dict))
+    return isinstance(object, (frozendict.frozendict, dict))
 
 
 Validator = jsonschema.validators.extend(
@@ -25,5 +28,29 @@ def is_json(object):
     json.loads(object)
     return True
 
+
 checker.checkers["json"] = is_json, (json.JSONDecodeError,)
 
+
+def localize_validator(lang):
+    if lang in LOCALIZED_VALIDATOR:
+        return LOCALIZED_VALIDATOR.get(lang)
+
+    LocalizedValidator = jsonschema.validators.extend(
+        jsonschema.Draft7Validator,
+        type_checker=jsonschema.Draft7Validator.TYPE_CHECKER.redefine(
+            "array", is_array
+        ).redefine("object", is_object),
+    )
+    LocalizedValidator.VALIDATORS = translate.translate(LocalizedValidator.VALIDATORS, lang)
+    LocalizedValidator.TYPE_CHECKER = jsonschema.TypeChecker(
+        {
+            **LocalizedValidator.TYPE_CHECKER._type_checkers,
+            **translate.translate(LocalizedValidator.TYPE_CHECKER._type_checkers, lang),
+        }
+    )
+    LOCALIZED_VALIDATOR[lang] = LocalizedValidator
+    return LOCALIZED_VALIDATOR[lang]
+
+
+LOCALIZED_VALIDATOR = {}
