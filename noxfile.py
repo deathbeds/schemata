ea7402a59ced1954577315b483545c66fc1a9b0a
tonyfast <tony.fast@gmail.com>
2021-04-05 16:35:26 -0400

    commit to build

--- noxfile.py
+++ noxfile.py
@@ -1,17 +1,37 @@
 try:
-    import nox, os
+    import nox, os, subprocess, functools
     CI = "GITHUB_ACTION" in os.environ
     @nox.session(python=False, reuse_venv=not CI)
     def build(session):
-        if CI:
+        try:
+            import flit
+        except:
             session.install(*"--upgrade pip wheel setuptools flit".split())
         "SOURCE_DATE_EPOCH=$(git log -1 --format=%ct)", 
-        session.run(*"flit build".split())
+        env=dict(
+            SOURCE_DATE_EPOCH=subprocess.check_output("git log -1 --format=%ct".split()).decode("utf-8").strip()
+        )
+        session.run(*"flit build".split(), env=env)
+
+    @nox.session(python=CI, reuse_venv=not CI)
+    def test(session):
+        try:
+            import pytest
+        except:
+            session.install(*"--ignore-installed --upgrade .[test,ci]".split())
+        session.run(*"pytest -vv --pyargs . --nbval -pno:importnb --durations=5 --cov=schemata --cov-report=term-missing:skip-covered --no-cov-on-fail".split())
+        session.run(*"coverage html".split())
 
     @nox.session(python=False)
-    def test(sesion):
-        session.install("pytest")
-
+    def docs(session):
+        try:
+            import jupyter_book
+        except:
+            session.install(*"--ignore-installed --upgrade .[docs]".split())
+        with open("conf.py", "w") as file:
+            session.run(*"jb config sphinx --toc docs/_toc.yml --config docs/_config.yml .".split(), stdout=file)
+            
+        session.run(*"jb build --toc docs/_toc.yml --config docs/_config.yml .".split())
 except ModuleNotFoundError:
     pass
 
