59a6039ad2b553c9b2ff048465bcb0c445b502cb
tonyfast <tony.fast@gmail.com>
2021-04-06 10:28:09 -0400

    rtd

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 exec(
     __import__("subprocess").check_output(
-        "jb config sphinx --toc _toc.yml --config _config.yml ..".split(),
+        "jb config sphinx --toc docs/_toc.yml --config docs/_config.yml .".split(),
     ), globals(), locals()
 )
