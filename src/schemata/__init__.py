3a320badc9ac26159589e49a5cd9c4579adff01b
tonyfast <tony.fast@gmail.com>
2021-06-08 13:52:28 -0400

    suppress rdf for the sake of inspoection, add pandas test

--- src/schemata/__init__.py
+++ src/schemata/__init__.py
@@ -1,6 +1,6 @@
 """composable types and traits in python"""
 __version__ = "0.0.1"
-from . import compat, ui
+from . import compat, ui, rdf
 from .arrays import *
 from .callables import *
 from .composites import *
@@ -8,7 +8,6 @@ from .files import *
 from .mediatypes import *
 from .numbers import *
 from .objects import *
-from .rdf import *
 from .strings import *
 from .templates import *
 from .times import *
